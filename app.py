c52f6f4795096d38dac6e7507a1f5e53d22d8896
unknown <jerobert11unicomtic@gmail.com>
2026-06-11 14:42:28 +0530

    templates files added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.get("/")
-def home():
-    return "HELLO FROM FLASK IN WINDOWS"
+@app. route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
